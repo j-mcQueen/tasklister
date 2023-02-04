5b89e6a26cc36331d3883abf64450613bc65b72b
jack mcqueen <jdmcqueen44@gmail.com>
2023-02-04 15:39:55 +0000

    Add basic markup

--- src/components/sidebar/Sidebar.js
+++ src/components/sidebar/Sidebar.js
@@ -1,7 +1,27 @@
 export default function Sidebar() {
     return (
         <div>
-            
+            <div id="categories">
+                <ul>
+                    <li>
+                        All
+                    </li>
+
+                    <li>
+                        Today
+                    </li>
+
+                    <li>
+                        Week
+                    </li>
+                </ul>
+            </div>
+
+            <div id="projects">
+                <p>
+                    Projects
+                </p>
+            </div>
         </div>
     )
 }
