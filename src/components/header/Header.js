7b6a531a96998704f0479359767cc046e90876b1
jack mcqueen <jdmcqueen44@gmail.com>
2023-02-04 14:30:53 +0000

    Import Login

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -2,11 +2,13 @@ import "./header.css";
 import "./burger.css";
 import "./login.css";
 import Burger from "./Burger"
+import Login from "./Login";
 
 export default function Header() {
     return (
         <header>
             <Burger/>
+            <Login/>
         </header>
     )
 }
