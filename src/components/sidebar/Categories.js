1edf9c539dbded1d95eee74e34e51eacea8750be
jack mcqueen <jdmcqueen44@gmail.com>
2023-02-05 16:33:15 +0000

    Add category title markup

--- src/components/sidebar/Categories.js
+++ src/components/sidebar/Categories.js
@@ -1,7 +1,9 @@
 export default function Categories() {
     return (
         <div id="categories">
-            
+            <h2>All</h2>
+            <h2>Today</h2>
+            <h2>Week</h2>
         </div>
     )
 }
