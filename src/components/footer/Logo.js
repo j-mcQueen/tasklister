a8004421229e753927de595ab866ca2d7ef283b3
jack mcqueen <jdmcqueen44@gmail.com>
2023-02-05 14:42:50 +0000

    Import octocat, set attributes on img

--- src/components/footer/Logo.js
+++ src/components/footer/Logo.js
@@ -1,7 +1,15 @@
+import octocat from "../../media/logos/github-mark-white.png";
+
 export default function Logo() {
     return (
         <div id="logo">
-            
+            <a href="https://github.com/j-mcQueen" target="_blank" rel="noopener noreferrer">
+                <img
+                    id="cat"
+                    src={octocat}
+                    alt="The Github cat logo"
+                />
+            </a>
         </div>
     )
 }
