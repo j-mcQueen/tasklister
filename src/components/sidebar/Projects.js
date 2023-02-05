80477eb26fa5ec9ac87d49592f7a0997cfa470b9
jack mcqueen <jdmcqueen44@gmail.com>
2023-02-05 16:29:42 +0000

    Implement title and add div markup

--- src/components/sidebar/Projects.js
+++ src/components/sidebar/Projects.js
@@ -1,7 +1,13 @@
 export default function Projects() {
     return (
         <div id="projects">
+            <h2>Projects</h2>
 
+            <div className="add">
+                <span id="plus">+</span>
+                <p className="insert">Add</p>
+                <ul></ul>
+            </div>
         </div>
     )
 }
