316fbbb1afc3cef3fbd7386a516f5dfae31e7b7a
jack mcqueen <jdmcqueen44@gmail.com>
2023-02-06 17:54:11 +0000

    Add btns with basic attr

--- src/components/header/forms/FormBtns.js
+++ src/components/header/forms/FormBtns.js
@@ -1,7 +1,19 @@
 export default function FormBtns() {
     return (
         <div id="btns">
-            
+            <button 
+                type="button"
+                id="cont"
+            >
+                continue
+            </button>
+
+            <button 
+                type="button"
+                id="cancel"
+            >
+                cancel
+            </button>
         </div>
     )
 }
