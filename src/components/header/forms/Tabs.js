44020ef43d1b8364b9410754f16fac7eec4435d7
jack mcqueen <jdmcqueen44@gmail.com>
2023-02-06 17:41:15 +0000

    Revert "Export basic Tabs function"
    This reverts commit 5163c689aef6d360bcf325e7d5449742c356e3ce.

--- src/components/header/forms/Tabs.js
+++ src/components/header/forms/Tabs.js
@@ -1,9 +1,7 @@
 export default function Tabs() {
     return (
-        <div id="tabs">
-            <span id="">login</span>
-            <span id="separator">|</span>
-            <span id="">sign up</span>
+        <div className="formModal">
+
         </div>
     )
 }
