5163c689aef6d360bcf325e7d5449742c356e3ce
jack mcqueen <jdmcqueen44@gmail.com>
2023-02-06 17:40:46 +0000

    Export basic Tabs function

--- src/components/header/forms/Tabs.js
+++ src/components/header/forms/Tabs.js
@@ -1,7 +1,9 @@
 export default function Tabs() {
     return (
-        <div className="formModal">
-
+        <div id="tabs">
+            <span id="">login</span>
+            <span id="separator">|</span>
+            <span id="">sign up</span>
         </div>
     )
 }
