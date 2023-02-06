babb447b1dcc1d73f97af7ba6cad73857feabc10
jack mcqueen <jdmcqueen44@gmail.com>
2023-02-06 17:48:07 +0000

    Import FormBtns, Tabs

--- src/components/header/forms/Modal.js
+++ src/components/header/forms/Modal.js
@@ -1,7 +1,11 @@
+import Tabs from "./Tabs"
+import FormBtns from "./FormBtns"
+
 export default function Modal() {
     return (
         <div id="modal">
-            
+            <Tabs/>
+            <FormBtns/>
         </div>
     )
 }
