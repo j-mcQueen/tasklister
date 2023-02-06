7afd95048a361ba7df19cd8d022684e83858a7f9
jack mcqueen <jdmcqueen44@gmail.com>
2023-02-06 17:53:39 +0000

    Add tabs and separator

--- src/components/header/forms/Tabs.js
+++ src/components/header/forms/Tabs.js
@@ -1,7 +1,9 @@
 export default function Tabs() {
     return (
         <div id="tabs">
-
+            <span>login</span>
+            <span>|</span>
+            <span>sign up</span>
         </div>
     )
 }
