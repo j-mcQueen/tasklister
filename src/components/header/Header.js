119c52c1cc267cc837e9ae197405565174075165
jack mcqueen <jdmcqueen44@gmail.com>
2023-02-06 17:47:12 +0000

    Import Modal

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -3,12 +3,14 @@ import "./burger.css";
 import "./login.css";
 import Burger from "./Burger"
 import Login from "./Login";
+import Modal from "./forms/Modal";
 
 export default function Header() {
     return (
         <header>
             <Burger/>
             <Login/>
+            <Modal/>
         </header>
     )
 }
