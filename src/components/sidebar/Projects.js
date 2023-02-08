fd042597111d910fcdcdf2cd58b8043952a04dab
jack mcqueen <jdmcqueen44@gmail.com>
2023-02-08 20:15:45 +0000

    Change span to button and add ul for added projects

--- src/components/sidebar/Projects.js
+++ src/components/sidebar/Projects.js
@@ -2,11 +2,15 @@ export default function Projects() {
     return (
         <div id="projects">
             <h2>Projects</h2>
+            <ul id="projects-list"></ul>
 
             <div className="add">
-                <span id="plus">+</span>
-                <p className="insert">Add</p>
-                <ul></ul>
+                <button 
+                    type="button"
+                    className="plus"
+                >
+                    add
+                </button>
             </div>
         </div>
     )
