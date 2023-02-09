eb41a6362091b1e9df21e00a0d4c1afeae0fa6f4
jack mcqueen <jdmcqueen44@gmail.com>
2023-02-09 16:16:15 +0000

    Set initial state and pass to Modal and Login along with updater

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,16 +1,23 @@
 import "./header.css";
 import "./burger.css";
 import "./login.css";
-import Burger from "./Burger"
+import Burger from "./Burger";
 import Login from "./Login";
 import Modal from "./forms/Modal";
+import { useState } from "react";
 
 export default function Header() {
+const [isActive, setIsActive] = useState(false);
     return (
         <header>
             <Burger/>
-            <Login/>
-            <Modal/>
+            <Login
+                cur={isActive}
+                next={setIsActive}
+            />
+            <Modal
+                cur={isActive}
+            />
         </header>
     )
 }
