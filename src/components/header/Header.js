4b43511e0a22fdb932e0f562070c08eea3a87b49
jack mcqueen <jdmcqueen44@gmail.com>
2023-02-09 16:17:23 +0000

    Revert "Set initial state and pass to Modal and Login along with updater"
    This reverts commit eb41a6362091b1e9df21e00a0d4c1afeae0fa6f4.

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,23 +1,16 @@
 import "./header.css";
 import "./burger.css";
 import "./login.css";
-import Burger from "./Burger";
+import Burger from "./Burger"
 import Login from "./Login";
 import Modal from "./forms/Modal";
-import { useState } from "react";
 
 export default function Header() {
-const [isActive, setIsActive] = useState(false);
     return (
         <header>
             <Burger/>
-            <Login
-                cur={isActive}
-                next={setIsActive}
-            />
-            <Modal
-                cur={isActive}
-            />
+            <Login/>
+            <Modal/>
         </header>
     )
 }
