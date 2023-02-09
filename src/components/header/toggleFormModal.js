9cf6339d72b4e5c8523fd162dd1f2d2d75bba7ed
jack mcqueen <jdmcqueen44@gmail.com>
2023-02-09 20:08:52 +0000

    Toggle modal display value depending on supplied parameters

--- src/components/header/toggleFormModal.js
+++ src/components/header/toggleFormModal.js
@@ -1,3 +1,12 @@
-export default function toggleModal() {
-
+export default function toggleFormModal(current, updater) {
+        const modal = document.getElementById("modal");
+        const login = document.getElementById("log");
+        if (!current) {
+            modal.style.display = "block";
+            login.setAttribute("disabled", "disabled");
+        } else {
+            modal.style.display = "none";
+            login.removeAttribute("disabled");
+        }
+        updater(!current);
 }
