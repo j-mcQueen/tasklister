29cf0799cb02f8d2a9a269d8912b3d92c4460e72
jack mcqueen <jdmcqueen44@gmail.com>
2023-02-09 20:10:22 +0000

    Supply state and updater to form buttons component

--- src/components/header/forms/Modal.js
+++ src/components/header/forms/Modal.js
@@ -2,11 +2,15 @@ import Tabs from "./Tabs"
 import FormBtns from "./FormBtns"
 import "./form.css";
 
-export default function Modal() {
+export default function Modal({current, updater}) {
     return (
         <div id="modal">
             <Tabs/>
-            <FormBtns/>
+            {/* login/ sign-up inputs displayed here */}
+            <FormBtns
+                current={current}
+                updater={updater}
+            />
         </div>
     )
 }
