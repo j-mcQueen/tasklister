621a6b10ca8d0bdfd1a4229ec57ee4a7a64e29a7
jack mcqueen <jdmcqueen44@gmail.com>
2023-02-09 21:05:25 +0000

    Decrease indent and add explanation comment

--- src/components/header/toggleFormModal.js
+++ src/components/header/toggleFormModal.js
@@ -1,12 +1,13 @@
 export default function toggleFormModal(current, updater) {
-        const modal = document.getElementById("modal");
-        const login = document.getElementById("log");
-        if (!current) {
-            modal.style.display = "block";
-            login.setAttribute("disabled", "disabled");
-        } else {
-            modal.style.display = "none";
-            login.removeAttribute("disabled");
-        }
-        updater(!current);
+    // needs to be a function since it needs to be called on the buttons with current and updater
+    const modal = document.getElementById("modal");
+    const login = document.getElementById("log");
+    if (!current) {
+        modal.style.display = "block";
+        login.setAttribute("disabled", "disabled");
+    } else {
+        modal.style.display = "none";
+        login.removeAttribute("disabled");
+    }
+    updater(!current);
 }
