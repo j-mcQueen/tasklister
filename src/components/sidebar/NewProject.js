453e35a61e836e680cf2265ab3ae1c94c8c81517
jack mcqueen <jdmcqueen44@gmail.com>
2023-02-13 19:37:44 +0000

    Add NewProject form into Projects on click

---
+++ src/components/sidebar/NewProject.js
@@ -0,0 +1,28 @@
+export default function NewProject() {
+    return (
+        <form>
+            <fieldset>
+                <label htmlFor=""></label>
+                <input type="text"/>
+            </fieldset>
+
+            <fieldset>
+                <button
+                    type="button"
+                    id="insert"
+                    onClick=""
+                >
+                    add
+                </button>
+                
+                <button
+                    type="button"
+                    id=""
+                    onClick=""
+                >
+                    cancel
+                </button>
+            </fieldset>
+        </form>
+    )
+}
--- src/components/sidebar/Projects.js
+++ src/components/sidebar/Projects.js
@@ -1,37 +1,33 @@
-export default function Projects() {
-    // on button click
-        // 1 call function which insertsBefore() a small form to before the add div
-            // form contains a text input, an add button, and a cancel button
-        
-        // if user clicks add, first we need to check and prevent empty input   
-            // if user has added text, text is pushed to an array,
-                // array data is rendered to the DOM
-        // if user clicks cancel, form is removed from the DOM
-    const handleClick = () => {
-        const cntr = document.getElementById("projects");
-        const add = document.querySelector(".add");
-        const projectForm = document.createElement("form");
-        projectForm.innerHTML = `
-            <label for="">
-            <input type="text" id="add-project">
-            <div>
-                <button type="button" id="">add</button>
-                <button type="button" class="cancel">cancel</button>
-            </div>
-        `;
-        cntr.insertBefore(projectForm, add);
-    }
+import { useState } from "react";
+import NewProject from "./NewProject";
 
+export default function Projects() {
+    // --OBJECTIVE: Render a form when the user clicks the form button
+        // --RUBBER DUCK
+            // When a user clicks the plus button, a form containing a label, a text input, and two buttons (add, cancel) will appear. I would like to avoid element creation and selection using pure JS, and would rather do the same thing in React. Currently, I am working through the best way to go about this. Perhaps calling a component on click of the plus button?
+        
+        // --PLAN
+            // The default state of the new project form will be false, since we don't want it to appear on load
+                // Therefore, getting it to display should work logically the same as the login button click showing the modal
+                    // parent component sets initial state
+                    // Plus button has click event which updates state
+                    // state is checked for when rendering the form, 
+                        // returns the component for the form if true, returns an empty element if false
+    const [displayed, setIsDisplayed] = useState(false);
     return (
         <div id="projects">
             <h2>Projects</h2>
             <ul id="projects-list"></ul>
 
+            {
+                displayed ? <NewProject/> : <></>
+            }
+
             <div className="add">
                 <button 
                     type="button"
                     className="plus"
-                    onClick={handleClick}
+                    onClick={() => setIsDisplayed(!displayed)}
                 >
                     +
                 </button>
