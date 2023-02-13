987a1cae5af0e674eb9645202cd8da16e3af9739
jack mcqueen <jdmcqueen44@gmail.com>
2023-02-13 16:54:19 +0000

    Set hover styles on tabs

--- src/components/header/forms/Tabs.js
+++ src/components/header/forms/Tabs.js
@@ -1,9 +1,15 @@
 export default function Tabs({setter}) {
     return (
         <div id="tabs">
-            <span onClick={() => setter(0)}>login</span>
+            <span
+                className="hoverable"
+                onClick={() => setter(0)}>login</span>
+
             <span>|</span>
-            <span onClick={() => setter(1)}>sign up</span>
+
+            <span 
+                className="hoverable" 
+                onClick={() => setter(1)}>sign up</span>
         </div>
     )
 }
