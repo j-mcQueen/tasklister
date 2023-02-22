44cd8bb151f9399afa1b257b4b217bbe52c1d0bc
jack mcqueen <jdmcqueen44@gmail.com>
2023-02-22 16:05:24 +0000

    Filter arena task array on complete icon click

--- src/components/arena/Arena.js
+++ src/components/arena/Arena.js
@@ -44,37 +44,33 @@ export default function Arena({ category, task, setTask }) {
                                 <div className="options">
                                     <div>
                                         <svg 
-                                            className="icon"
+                                            className="remove icon"
+                                            onClick={() => {
+                                                setArenaTask(
+                                                    arenaTask.filter(item =>
+                                                        item.id !== task.id
+                                                    )
+                                                )
+                                            }}
                                             xmlns="http://www.w3.org/2000/svg"
                                             viewBox="0 0 16 16">
                                                 <path 
-                                                    fill-rule="evenodd"
+                                                    fillRule="evenodd"
                                                     d="M1.5 8a6.5 6.5 0 1113 0 6.5 6.5 0 01-13 0zM0 8a8 8 0 1116 0A8 8 0 010 8zm11.78-1.72a.75.75 0 00-1.06-1.06L6.75 9.19 5.28 7.72a.75.75 0 00-1.06 1.06l2 2a.75.75 0 001.06 0l4.5-4.5z">
                                                 </path>
                                         </svg>
                                     </div>
                                     <div>
                                         <svg
-                                            className="icon" 
+                                            className="edit icon" 
                                             xmlns="http://www.w3.org/2000/svg" 
                                             viewBox="0 0 20 20">
                                                 <path 
                                                     d="M17.414 2.586a2 2 0 00-2.828 0L7 10.172V13h2.828l7.586-7.586a2 2 0 000-2.828z">
                                                 </path>
                                                 <path 
-                                                    fill-rule="evenodd"
+                                                    fillRule="evenodd"
                                                     d="M2 6a2 2 0 012-2h4a1 1 0 010 2H4v10h10v-4a1 1 0 112 0v4a2 2 0 01-2 2H4a2 2 0 01-2-2V6z" clipRule="evenodd">
-                                                </path>
-                                        </svg>
-                                    </div>
-                                    <div>
-                                        <svg 
-                                            className="remove icon"
-                                            xmlns="http://www.w3.org/2000/svg" 
-                                            viewBox="0 0 20 20">
-                                                <path 
-                                                    fillRule="evenodd" 
-                                                    d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clipRule="evenodd">
                                                 </path>
                                         </svg>
                                     </div>
