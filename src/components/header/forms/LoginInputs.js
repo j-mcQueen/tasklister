68a0bb5beb3536a1962c2ab812dde1e771b47ead
jack mcqueen <jdmcqueen44@gmail.com>
2023-05-17 17:32:38 -0400

    Remove google oauth

--- src/components/header/forms/LoginInputs.js
+++ src/components/header/forms/LoginInputs.js
@@ -1,25 +1,68 @@
-import FormBtns from "./FormBtns"
-export default function LoginInputs({isActive, setIsActive}) {
-    return(
-        <form>
-            <fieldset>
-                <div>
-                    <label htmlFor="uname">Username *</label>
-                    <input type="text" id="uname" required/>
-                </div>
-            </fieldset>
+import FormBtns from "./FormBtns";
+import { useState } from "react";
+import supabase from "../../../supabase/setup";
+export default function LoginInputs({ isActive, setIsActive }) {
+  const [authError, setAuthError] = useState(false);
+  const errorString =
+    "Invalid username and password. Please check your credentials and try again.";
 
-            <fieldset>
-                <div>
-                    <label htmlFor="pwd">Password *</label>
-                    <input type="password" id="pwd" required/>
-                </div>
-            </fieldset>
+  const [values, setValues] = useState({
+    email: "",
+    pwd: "",
+  });
 
-            <FormBtns
-                isActive={isActive}
-                setIsActive={setIsActive}
-            />
-        </form>
-    )
+  const handleChange = (e) => {
+    setValues({ ...values, [e.target.id]: e.target.value });
+  };
+
+  const handleSubmit = async (e) => {
+    // because of the async code, form submits and refreshes page before async request resolves
+    const { data, error } = await supabase.auth.signInWithPassword({
+      email: values.email,
+      password: values.pwd,
+    });
+    if (error) {
+      e.preventDefault();
+      console.log(error);
+      setAuthError(true);
+      return false;
+    } else if (data) {
+      console.log(data);
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      {authError ? <p style={{ color: "red" }}>{errorString}</p> : null}
+      <fieldset>
+        <div>
+          <label htmlFor="uname">Email *</label>
+          <input
+            type="email"
+            id="email"
+            onChange={handleChange}
+            minLength="3"
+            autoComplete="off"
+            required
+          />
+        </div>
+      </fieldset>
+
+      <fieldset>
+        <div>
+          <label htmlFor="pwd">Password *</label>
+          <input
+            type="password"
+            id="pwd"
+            onChange={handleChange}
+            minLength={8}
+            autoComplete="off"
+            required
+          />
+        </div>
+      </fieldset>
+
+      <FormBtns isActive={isActive} setIsActive={setIsActive} />
+    </form>
+  );
 }
